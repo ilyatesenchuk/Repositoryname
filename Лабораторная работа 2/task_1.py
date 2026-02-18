be3a61653e9c554d995f668322fc7604d05acceb
ilyatesenchuk <ilya_tesenchuk@mail.ru>
2026-02-18 19:03:34 +0500

    task_1.py

--- Лабораторная работа 2/task_1.py
+++ Лабораторная работа 2/task_1.py
@@ -1,14 +1,3 @@
-class Book:
-    def __init__(self, id_, name, pages):
-        self.id = id_
-        self.name = name
-        self.pages = pages
-
-    def __str__(self):
-        return f'Книга "{self.name}"'
-
-    def __repr__(self):
-        return f"Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})"
 BOOKS_DATABASE = [
     {
         "id": 1,
@@ -24,7 +13,17 @@ BOOKS_DATABASE = [
 
 
 # TODO написать класс Book
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
 
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f"Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})"
 
 if __name__ == '__main__':
     # инициализируем список книг
